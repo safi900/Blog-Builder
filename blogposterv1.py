46780a3d0873880a29f6ab9cec06f307eec265ba
Mustapha Safadieh <34229183+safi900@users.noreply.github.com>
2018-11-06 19:19:42 -0500

    Add files via upload
    Prototype

---
+++ blogposterv1.py
@@ -0,0 +1,7 @@
+
+
+date = input("Date (mm/dd/yy): ")
+heading = input("Heading: ")
+post = input("Post Text Here: ")
+
+print("<p><b>" + date + " - <i>" + heading + "</b></i><br> " + post + "</p>")
